6931d6b186cb5f2898a0ee27e8aa3d282b122abb
Ankit Kumar <113016851+ankitkumar-2@users.noreply.github.com>
2024-09-03 16:27:24 +0000

    FirstCommit

---
+++ ColorCoding.cpp
@@ -0,0 +1,32 @@
+#include "ColorCoding.h"
+namespace TelCoColorCoder
+{
+
+ ColorPair::ColorPair(MajorColor major, MinorColor minor):
+    majorColor(major), minorColor(minor)
+{}
+            MajorColor ColorPair::getMajor() {
+                return majorColor;
+            }
+            MinorColor ColorPair::getMinor() {
+                return minorColor;
+            }
+            std::string ColorPair::ToString() {
+                std::string colorPairStr = MajorColorNames[majorColor];
+                colorPairStr += " ";
+                colorPairStr += MinorColorNames[minorColor];
+                return colorPairStr;
+            }
+ 
+    ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
+}
